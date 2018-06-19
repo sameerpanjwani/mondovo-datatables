f5375a6b80fe74404009118d6b7775ad8cb64c73
Jestin <jestin@maximizer-e-services.com>
2018-06-19 13:49:36 +0530

    Updates in parameters

--- src/config/mondovo-datatable.php
+++ src/config/mondovo-datatable.php
@@ -16,8 +16,8 @@ return [
 	'default_logo_url' => 'img/mondovo-logo.png',
 	'demo_user' => false,
 	'access_level' => 1,
-	'manager_url' => 1,
-	'kd_modal_url' => 1,
+	'manager_url' => 0,
+	'kd_modal_url' => 0,
 	'stop_words_list' => [
 		file_get_contents('https://app.mondovo.live/dummy_files/onPage/stopWords/stop_words.txt'),
 		file_get_contents('https://app.mondovo.live/dummy_files/onPage/stopWords/stop_words_first.txt')
